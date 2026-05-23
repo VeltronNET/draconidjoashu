38517754e3074ea50920b4d8d18e6e045111e3ad
DraconidJo <186290181+draconidjo@users.noreply.github.com>
2026-05-23 18:18:57 -0400

    JavaScript testing

---
+++ JS/Pokemon.js
@@ -0,0 +1,12 @@
+let pokemon = [
+     "bulbasaur",
+     "charmander",
+     "squirtle"
+   ];
+
+console.log("size: "+pokemon.length);
+console.log("All pokémon list:");
+
+for(let i = 0; i < pokemon.length; i++){
+   console.log("pokémon name: "+pokemon[i]);
+}
